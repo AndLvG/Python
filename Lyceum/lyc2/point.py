8a8181b5ed033ccde714546285d679d34b276793
AndLvG <realandmir@gmail.com>
2019-10-03 14:13:56 +0300

    bugs

--- Lyceum/lyc2/point.py
+++ Lyceum/lyc2/point.py
@@ -1,8 +1,7 @@
 class Point:
     def __init__(self, name, x, y):
         self.name = name
-        self.x = x
-        self.y = y
+        self.x, self.y = x, y
         self.cords = (self.x, self.y)
 
     def __invert__(self):
@@ -20,6 +19,9 @@ class Point:
     def get_coords(self):
         return self.cords
 
+    def __repr__(self):
+        return f"Point('{self.name}', {self.x}, {self.y})"
+
 
 class ColoredPoint(Point):
     def __init__(self, name, x, y, rgb=(0, 0, 0)):
@@ -36,3 +38,9 @@ class ColoredPoint(Point):
 
     def get_color(self):
         return self.rgb
+
+
+# points = [Point('A', 0, 3), Point('B', 4, 0)]
+# print(points)
+# print(points[0])
+# print(repr(points[0]))
