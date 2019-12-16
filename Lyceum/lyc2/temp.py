26f6372fadb5fedf85a7a5fea1aa6bbbe724cc47
Andrey <realandmir@gmail.com>
2019-12-16 21:34:49 +0300

    from note

--- Lyceum/lyc2/temp.py
+++ Lyceum/lyc2/temp.py
@@ -1,4 +1,5 @@
 import pygame
+<<<<<<< Updated upstream
 import math
 from pygame.locals import *
 SIZE = 800, 800
@@ -26,3 +27,48 @@ while not done:
     pygame.display.flip()
     degree+=2
     FPSCLOCK.tick(60)
+=======
+
+
+class Board:
+    # создание поля
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        # значения по умолчанию
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    # настройка внешнего вида
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self):
+        for x in range(self.left, self.width * self.cell_size +self.left, self.cell_size):
+            for y in range(self.top, self.height * self.cell_size+self.top, self.cell_size):
+                pygame.draw.rect(screen, (0, 255, 0), (x, y, self.cell_size, self.cell_size), 1)
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell)
+
+
+pygame.init()
+board = Board(4, 3)
+board.set_view(40,40, 30)
+screen = pygame.display.set_mode((501, 501))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            board.get_click(event.pos)
+    screen.fill((0, 0, 0))
+    board.render()
+    pygame.display.flip()
+>>>>>>> Stashed changes
