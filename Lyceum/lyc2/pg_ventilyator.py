300b79f97b404561b166255a4c1412fbebcceedc
AndLvG <realandmir@gmail.com>
2019-12-22 15:40:01 +0300

    from work

--- Lyceum/lyc2/pg_ventilyator.py
+++ Lyceum/lyc2/pg_ventilyator.py
@@ -11,12 +11,12 @@ surf = pygame.Surface((201, 201))
 surf.fill(pygame.Color('black'))
 x0 = y0 = 101
 a = 70
-b = int(a * math.sin(math.radians(30) / 2))
 
-x1 = x0 - b
-x2 = x0 + b
-y1 = y2 = y0 - int(math.sqrt(a ** 2 - b ** 2))
-pygame.draw.polygon(surf, pygame.Color('white'), [(x0, y0), (x1, y1), (x2, y2)], 0)
+xn1 = x0 - int(math.sin(math.radians(- 15)) * a)
+yn1 = y0 - int(math.cos(math.radians(- 15)) * a)
+xn2 = x0 - int(math.sin(math.radians(15)) * a)
+yn2 = y0 - int(math.cos(math.radians(15)) * a)
+pygame.draw.polygon(surf, pygame.Color('white'), [(x0, y0), (xn1, yn1), (xn2, yn2)], 0)
 
 xn1 = x0 - int(math.sin(math.radians(120 - 15)) * a)
 yn1 = y0 - int(math.cos(math.radians(120 - 15)) * a)
@@ -31,28 +31,29 @@ yn2 = y0 - int(math.cos(math.radians(-120 + 15)) * a)
 pygame.draw.polygon(surf, pygame.Color('white'), [(x0, y0), (xn1, yn1), (xn2, yn2)], 0)
 
 pygame.draw.circle(surf, pygame.Color('white'), (x0, y0), 10)
-angle = 0
+degree = angle = 0
+fps = 600
+v = 50  # градусов в секунду
 
 while not done:
-    clock.tick(10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         if event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
-                angle += 5
+                angle += 1
             elif event.button == 3:
-                angle -= 5
+                angle -= 1
 
     rotatedSurf = pygame.transform.rotate(surf, degree)
     rotRect = rotatedSurf.get_rect()
     rotRect.center = (x0, y0)
     screen.blit(rotatedSurf, rotRect)
 
-    degree += angle
+    degree += angle * v / fps
     if degree < 0:
         degree = 360
-
+    clock.tick(fps)
     pygame.display.flip()
 
 pygame.quit()
