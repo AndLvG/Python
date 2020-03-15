16adfc25af299785513ac3462fb53aa21df9235f
Lvov <realandmir@gmail.com>
2020-03-15 18:27:15 +0300

    from note

--- Lyceum/lyc2/web6_api_poisk_apteka.py
+++ Lyceum/lyc2/web6_api_poisk_apteka.py
@@ -10,7 +10,7 @@ from PIL import Image
 # Тогда запрос к геокодеру формируется следующим образом:
 # 'Москва, ул. Ак. Королева, 12'  # " ".join(sys.argv[1:])
 toponym_to_find = input("Введите адрес (например 'Москва, ул. Ак. Королева, 12'): ")
-delta = input("Введите размер в градусах (например 0.05")  # "0.05"
+delta = input("Введите размер в градусах (например 0.05): ")  # "0.05"
 
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 geocoder_params = {
@@ -34,17 +34,52 @@ toponym_coodrinates = toponym["Point"]["pos"]
 # Долгота и широта:
 toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
 
+search_api_server = "https://search-maps.yandex.ru/v1/"
+api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+
+address_ll =",".join([toponym_longitude, toponym_lattitude])
+
+search_params = {
+    "apikey": api_key,
+    "text": "аптека",
+    "lang": "ru_RU",
+    "ll": address_ll,
+    "type": "biz"
+}
+
+response = requests.get(search_api_server, params=search_params)
+if not response:
+    #...
+    pass
+# Преобразуем ответ в json-объект
+json_response = response.json()
+
+# Получаем первую найденную организацию.
+organization = json_response["features"][0]
+# Название организации.
+org_name = organization["properties"]["CompanyMetaData"]["name"]
+# Адрес организации.
+org_address = organization["properties"]["CompanyMetaData"]["address"]
+
+# Получаем координаты ответа.
+point = organization["geometry"]["coordinates"]
+org_point = "{0},{1}".format(point[0], point[1])
+
 # Собираем параметры для запроса к StaticMapsAPI:
 map_params = {
-    "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": ",".join([delta, delta]),
+    # позиционируем карту центром на наш исходный адрес
+    "ll": address_ll,
+    # "spn": ",".join([delta, delta]),
     "l": "map",
-    "pt": ",".join([toponym_longitude, toponym_lattitude]) + ",comma"
+    # добавим точку, чтобы указать найденную аптеку
+    "pt": "{0},pm2dgl".format(org_point) + "~" + ",".join([toponym_longitude, toponym_lattitude]) + ",comma"
 }
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 # ... и выполняем запрос
 response = requests.get(map_api_server, params=map_params)
+
+
 print(response.url)
 Image.open(BytesIO(
     response.content)).show()
