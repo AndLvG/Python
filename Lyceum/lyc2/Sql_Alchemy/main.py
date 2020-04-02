dc81962fc0c42dc94b1871f0ca45b3d45e8c8c17
Lvov <realandmir@gmail.com>
2020-04-02 22:48:08 +0300

    from note

--- Lyceum/lyc2/Sql_Alchemy/main.py
+++ Lyceum/lyc2/Sql_Alchemy/main.py
@@ -3,11 +3,11 @@ from data import db_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-db_session.global_init("db/blogs.sqlite")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/blogs.sqlite'
+
 
 def main():
-
-    
+    db_session.global_init("blogs.sqlite")
     app.run()
 
 
