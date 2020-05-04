5ceb602e01214818923bd3bcc7a7b3645357ae52
AndLvG <realandmir@gmail.com>
2020-05-04 10:31:19 +0300

    from work

--- Lyceum/Mars_Sql_Alchemy/main.py
+++ Lyceum/Mars_Sql_Alchemy/main.py
@@ -2,13 +2,10 @@ from data.__all_models import *
 from flask import Flask, render_template, redirect, url_for, request
 from data import db_session
 from data.users import User, Anonymous
-from data.jobs import Jobs
 from data.login import LoginForm
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from data.register import RegisterForm
-from data.add_job import AddJobForm
-
 
 colonists = [{"surname": "Scott",
               "name": "Ridley",
@@ -49,17 +46,19 @@ login_manager.init_app(app)
 login_manager.anonymous_user = Anonymous
 login_manager.login_view = 'login'
 
+
 @app.route("/")
 @app.route('/index')
 @login_required
 def index():
-    return redirect(url_for('jobs_table'))
-    # return render_template('index.html', title='Экспедиция на Марс')
+    return render_template('index.html', title='Экспедиция на Марс')
+
 
 @login_manager.user_loader
 def load_user(user_id):
     session = db_session.create_session()
     return session.query(User).get(user_id)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -85,18 +84,42 @@ def login():
 def success():
     return render_template('success_login.html', title='Удачный вход')
 
+
 @app.route('/logout')
-@login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route("/jobs_table")
-@login_required
+
+@app.route("/jobs_table", methods=['GET', 'POST'])
 def jobs_table():
     session = db_session.create_session()
-    j = session.query(Jobs).all()
-    return render_template("jobs_table.html", jobs=j, title="Журнал работ")
+    if request.method == 'GET':
+        j = session.query(jobs.Jobs).all()
+        return render_template("jobs_table.html", jobs=j, title="Журнал работ")
+    elif request.method == 'POST':
+        if request.form['action'] == 'text' and request.form['search_str'] != "":
+            j = session.query(jobs.Jobs).filter(jobs.Jobs.job.like(f"%{request.form['search_str']}%"))
+
+        elif request.form['action'] == 'privat':
+            option = request.form['priv']
+            if option == 'all':
+                j = session.query(jobs.Jobs).all()
+            elif option == 'my':
+                j = session.query(jobs.Jobs).filter(jobs.Jobs.is_finished == 0)
+            elif option == 'other':
+                j = session.query(jobs.Jobs).filter(jobs.Jobs.is_finished == 1)
+
+        elif request.form['action'] == 'data':
+            j = session.query(jobs.Jobs).filter(
+                jobs.Jobs.start_date.between(request.form['datemin'], request.form['datemax'])).order_by(
+                jobs.Jobs.start_date.desc())
+
+        else:
+            j = session.query(jobs.Jobs).all()
+
+        return render_template("jobs_table.html", jobs=j, title="Журнал работ")
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
@@ -123,78 +146,9 @@ def reqister():
         user.set_password(form.password.data)
         session.add(user)
         session.commit()
-        return redirect(url_for('index'))
+        return redirect('/success')
     return render_template('register.html', title='Регистрация', form=form)
 
-@app.route('/addjob', methods=['GET', 'POST'])
-@login_required
-def addjob():
-    form = AddJobForm()
-    session = db_session.create_session()
-    form.team_leader.choices = session.query(User.id, User.name).all()
-    if form.validate_on_submit():
-
-        job = Jobs(
-            team_leader=int(form.team_leader.data),
-            job=form.job.data,
-            work_size=int(form.work_size.data),
-            collaborators=form.collaborators.data,
-            start_date=form.start_date.data,
-            end_date=form.end_date.data,
-            is_finished=form.is_finished.data
-        )
-
-        session.add(job)
-        session.commit()
-        return redirect(url_for('jobs_table'))  # redirect('/jobstable')
-    return render_template('addjob.html', title='Добавление нового задания', form=form)
-
-
-@app.route('/editjob/<int:id>', methods=['GET', 'POST'])
-@login_required
-def edit_job(id):
-    form = AddJobForm()
-    if request.method == "GET":
-        session = db_session.create_session()
-        j = session.query(Jobs).filter(Jobs.id == id).first()
-        if j:
-            form.team_leader.data = j.team_leader
-            job = form.job.data = j.job
-            form.work_size.data = j.work_size
-            form.collaborators.data = j.collaborators
-            form.start_date.data = j.start_date
-            form.end_date.data = j.end_date
-            form.is_finished.data = j.is_finished
-        # else:
-        #     abort(404)
-    if form.validate_on_submit():
-        session = db_session.create_session()
-        j = session.query(Jobs).filter(Jobs.id == id).first()
-        if j:
-            j.team_leader = int(form.team_leader.data)
-            j.job = form.job.data
-            j.work_size = int(form.work_size.data)
-            j.collaborators = form.collaborators.data
-            j.start_date = form.start_date.data
-            j.end_date = form.end_date.data
-            j.is_finished = form.is_finished.data
-            session.commit()
-            return redirect(url_for('jobs_table'))
-        # else:
-        #     abort(404)
-    return render_template('addjob.html', title='Редактирование задания', form=form)
-
-@app.route('/job_delete/<int:id>', methods=['GET', 'POST'])
-@login_required
-def job_delete(id):
-    session = db_session.create_session()
-    j = session.query(Jobs).filter(Jobs.id == id).first()
-    if j:
-        session.delete(j)
-        session.commit()
-    # else:
-    #     abort(404)
-    return redirect(url_for('jobs_table'))
 
 def main():
     db_session.global_init(DB)
